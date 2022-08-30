0696d4db58481fff4aa893203365c2b05fcb46df
Vraj Shah <vraj@bluelearn.in>
2022-08-30 20:07:46 +0530

    feat: add hero & sponsor section

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,113 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import typographyLogo from "./assets/logo_typo.svg";
+	import meciaMono from "./assets/logo_mono.svg";
+	import hcsLogo from "./assets/hcs_logo.svg";
+	import devfolio from "./assets/devfolio.svg";
+	import polygon from "./assets/polygon.svg";
+	import filecoin from "./assets/filecoin.svg";
+	import solana from "./assets/solana.svg";
+	import replit from "./assets/replit.svg";
+	import Icon from "@iconify/svelte";
+
+	let sponsors = ["devfolio", "polygon", "filecoin", "solana", "replit"];
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<svelte:head>
+	<script defer async src="https://apply.devfolio.co/v2/sdk.js"></script>
+</svelte:head>
+<div id="home">
+	<header>
+		<img width="60px" src={meciaMono} alt="MECIA Hacks logo" />
+		<img src={hcsLogo} alt="Hack Club SVIT logo" />
+		<!-- <img width="80" class="svit_logo" src="/svit_logo.png" alt="" /> -->
+	</header>
+	<main class="hero">
+		<img class="typo_logo" src={typographyLogo} alt="MECIA Hacks logo" />
+		<p class="subtext">an in-person multidisciplinary hackathon</p>
+		<div
+			class="apply-button"
+			data-hackathon-slug="hacksvit2022"
+			data-button-theme="dark-inverted"
+			style="height: 44px; width: 312px"
+		/>
+		<div class="icon_text_set">
+			<Icon width="24" icon="carbon:location" />
+			SVIT, Vasad
+		</div>
+	</main>
+</div>
+<section id="sponsors">
+	<div class="container">
+		<h1 style="margin-bottom: 64px;">Sponsors</h1>
+		<div class="sponsor_grid">
+			{#each sponsors as sponsor}
+				<!-- svelte-ignore missing-declaration -->
+				<img class="sponsor_img" src={eval(sponsor)} alt="" />
+			{/each}
+		</div>
+	</div>
+</section>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+	}
+	#home {
+		display: flex;
+		flex-direction: column;
+		gap: 20vh;
+		align-items: center;
+		width: 100%;
+		padding: 1.8em 1em;
+		min-height: 100vh;
+	}
+	.hero {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 24px;
+	}
+	.typo_logo {
+		width: 90%;
+		max-width: 32em;
+	}
+	.subtext {
+		font-family: var(--sans-serif);
+		max-width: 640px;
+		text-align: center;
+		font-size: 120%;
+	}
+	.icon_text_set {
+		font-family: var(--sans-serif);
+		display: flex;
+		align-items: center;
+		gap: 12px;
+		font-size: 120%;
+		max-width: 240px;
+		text-align: center;
+	}
+	#sponsors {
+		padding: 2.4em;
+		text-align: center;
+		background-color: #16062d;
+	}
+	.sponsor_grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+		place-items: center;
+		row-gap: 48px;
+		column-gap: 24px;
+	}
+	.sponsor_img {
+		width: 260px;
+	}
+	footer {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
 </style>
